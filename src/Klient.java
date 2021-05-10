6db2c14a853632f1aaa328eff683fcc4a5b3d497
Maciek-Szczesny <m.szcze15@gmail.com>
2021-05-10 20:28:19 +0200

    The functionality of automatically sending a set of numbers to the server and from server to client has been added

--- src/Klient.java
+++ src/Klient.java
@@ -12,48 +12,36 @@ public class Klient {
     public static final String HOST = "127.0.0.1";
 
     public static void main(String[] args) throws IOException {
-            //for (int i = 0; i < 5; i++) {
                  VmAlphaSetOfNumbers clientNumbers = new VmAlphaSetOfNumbers();
                  Set<Integer> clientNumbersValue = clientNumbers.getGeneratedNumbers();
                  List<Integer> mainListClient = new ArrayList<>();
                  mainListClient.addAll(clientNumbersValue);
-                //String [] clientArrayNumbers = mainListClient.toArray(String []::new);
-                String listString = mainListClient.stream().map(Object::toString).collect(Collectors.joining(","));
-
+                 String listString = mainListClient.stream().map(Object::toString).collect(Collectors.joining(","));
 
                  //nawiazanie polaczenia z serwerem
                 Socket sock = new Socket(HOST, PORT);
                 System.out.println("Nawiazalem polaczenie: " + sock);
 
-                //tworzenie strumieni danych i dostarczanie do socketu
-               // BufferedReader klaw = new BufferedReader(new InputStreamReader(System.in));
-               // PrintWriter outp = new PrintWriter(sock.getOutputStream());
 
-                OutputStream outputStream = sock.getOutputStream();
-                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-                dataOutputStream.writeBytes(listString);
-                dataOutputStream.flush();
-                dataOutputStream.close();
-                //przekazywanie danych  do strumienia
-               // System.out.print("Podaj liczby do skreslenia : ");
-                //String str = klaw.readLine();
-                //outp.println(str);
-                //outp.flush();
+
+        //tworzenie strumieni danych i dostarczanie do socketu
+         BufferedReader klaw = new BufferedReader(new InputStreamReader(System.in));
+         PrintWriter outp = new PrintWriter(sock.getOutputStream());
+        outp.println(listString);
+        outp.flush();
 
 
         //tworzenie strumienia danych pobieranych z gniazda sieciowego
         BufferedReader inp = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-
         //Odczyt danych ze strumienia
         int correctHits;
         correctHits = inp.read();
         System.out.println("Liczba prawidlowych trafien " + correctHits);
 
 
-
         try {
             FileWriter myWriter = new FileWriter("wygrane.txt", true);
-            myWriter.write("Ilosc trafien "+correctHits+ " dla zestawu liczb "+ mainListClient);
+            myWriter.write("Ilosc trafien "+correctHits+ " dla zestawu liczb "+ listString);
             myWriter.write(System.getProperty("line.separator"));
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
@@ -61,11 +49,10 @@ public class Klient {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
+        //dataOutputStream.close();
             //zamykanie polaczenia
-            //klaw.close();
-           // outp.close();
-           // sock.close();
+
+            sock.close();
     }
 }
 
--- src/Serwer.java
+++ src/Serwer.java
@@ -12,9 +12,11 @@ public class Serwer
     {
             VmAlphaSetOfNumbers serwerNumbers = new VmAlphaSetOfNumbers();
             Set<Integer> serwerNumbersValue = serwerNumbers.getGeneratedNumbers();
+            ServerSocket serv = new ServerSocket(PORT);
+
             for (int i = 0; i < 5; i++) {
-                //tworzenie gniazda serwerowego
-                ServerSocket serv = new ServerSocket(PORT);
+                //torrent gniazda serwerowego
+
 
                 //oczekiwanie na polaczenie i tworzenie gniazda sieciowego
                 System.out.println("Nasluchuje: " + serv);
@@ -28,7 +30,7 @@ public class Serwer
                 String clientNumbersAlpha;
                 clientNumbersAlpha = inp.readLine();
                 System.out.println("Liczby maszyny Bravo (gracz) " + clientNumbersAlpha);
-                List<String> clientList = List.of(clientNumbersAlpha.split(","));
+                //List<String> clientList = List.of(clientNumbersAlpha.split(","));
 
                 // Client list of numbers
                 String[] convertedClientArray = clientNumbersAlpha.split(",");
@@ -38,45 +40,30 @@ public class Serwer
                       convertedClientList.add(Integer.parseInt(number.trim()));
                     }
 
-                // Serwer list of numbers
+                // Liczby po stronie serwera
                 List<Integer> mainList = new ArrayList<>();
                 mainList.addAll(serwerNumbersValue);
-                System.out.println("List of integers from server " + mainList);
+                System.out.println("Zwycięskie liczby " + mainList);
                 mainList.retainAll(convertedClientList);
                 List<Integer> commonNumbers = mainList;
-                // Comparing two lists
-                System.out.println("Lista podobnych liczb "+commonNumbers);
+                // Porownywanie list
+                System.out.println("Lista trafionych liczb "+commonNumbers);
                 Integer numberOfHits = commonNumbers.size();
-                //valid deletions
-                System.out.println("Liczba trafień " + numberOfHits +" /6");
+                System.out.println("Liczba trafień " + numberOfHits +"/6");
 
-
-
-    /*            //Integer stringNumberOfHits = numberOfHits;
-                String stringNumberOfHits = numberOfHits.toString();
-
-                //InputStream klaw2 = new ByteArrayInputStream(stringNumberOfHits.getBytes(StandardCharsets.UTF_8));
-                InputStream klaw2 = sock.getInputStream();
-
-                PrintWriter outp = new PrintWriter(sock.getOutputStream());
-
-                //przekazywanie danych  do strumienia
-                System.out.print("ilosc dobrych skreslen : ");
-                //String str = klaw.readLine();
-                outp.println(klaw2);
-                outp.flush();*/
-
+                //Wysylanie poprzez strumien danych informacji o ilosci trafien
+                //Socket sockt = serv.accept();
                 OutputStream outputStream = sock.getOutputStream();
                 DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
                 dataOutputStream.write(numberOfHits);
                 dataOutputStream.flush();
-                dataOutputStream.close();
+               // dataOutputStream.close();
 
 
                 //zamykanie polaczenia
-                inp.close();
-                sock.close();
-                serv.close();
+//                inp.close();
+//                sock.close();
+//                serv.close();
         }
 
     }
